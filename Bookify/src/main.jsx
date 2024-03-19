7df9433aa9e2f2d598ea85708166ce904c6170a6
keearn <keearn96@gmail.com>
2024-03-19 16:06:54 +0800

    seperate into pages folder

--- Bookify/src/main.jsx
+++ Bookify/src/main.jsx
@@ -5,15 +5,16 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FullReadList from "./component/FullReadList.jsx";
+import FullReadList from "./pages/FullReadList.jsx";
 import RefetchLayout from "./component/RefetchLayout.jsx";
+import Index from "./pages/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<RefetchLayout />}>
-          <Route index element={<App />} />
+          <Route index element={<Index />} />
           <Route path="/fullreadlist" element={<FullReadList />} />
         </Route>
       </Routes>
---
+++ Bookify/src/pages/FullReadList.jsx
@@ -0,0 +1,89 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ReadItem from "../component/ReadItem";
+import Airtable from "airtable";
+import RefetchContext from "../component/RefetchContext";
+import styles from "../component/fullread.module.css";
+
+const FullReadList = () => {
+  const [fullReadList, setFullReadList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Step 1: Add loading state
+  const { refetchTrigger, setRefetchTrigger, toggleRefetch } =
+    useContext(RefetchContext);
+
+  const base = new Airtable({
+    apiKey: import.meta.env.VITE_API_KEY_AIRTABLE,
+  }).base("appSwRUsjekOwOCZ6");
+
+  useEffect(() => {
+    let ignore = false; //when ignore is true stop, prevents double data
+    setIsLoading(true);
+    base("Table 1")
+      .select()
+      .eachPage(
+        (records, fetchNextPage) => {
+          if (ignore) return;
+          setFullReadList((prevRecords) => [...prevRecords, ...records]);
+          if (records.length > 0) {
+            fetchNextPage();
+          } else {
+            setIsLoading(false);
+          }
+        },
+        (error) => {
+          if (error) {
+            console.error(error);
+          }
+          if (!ignore) {
+            setIsLoading(false);
+          }
+        }
+      );
+    return () => {
+      ignore = true;
+    };
+  }, [refetchTrigger]);
+
+  return (
+    <div className="fullRead">
+      <div className="fullReadHeader">
+        <Link to="/" className="logoImage">
+          <img
+            src="https://i.ibb.co/tMw6Btg/Screenshot-2024-03-17-at-22-13-19.png"
+            alt="Logo"
+            border="0"
+          />
+        </Link>
+        <div className="subFullReadHeader">Your Entire Read List</div>
+      </div>
+      <div className="fullReadWrapper">
+        <div className="fullReadingList">
+          {isLoading ? (
+            <div>
+              <a>
+                <img
+                  src="https://s9.gifyu.com/images/SUoFD.gif"
+                  alt="output onlinegiftools"
+                  border="0"
+                />
+              </a>
+            </div> // Step 4: Display loading indicator
+          ) : (
+            fullReadList.map((record) => (
+              <ReadItem
+                key={record.id}
+                id={record.id}
+                title={record.fields.Title}
+                author={record.fields.Author}
+                thumbnail={record.fields.Thumbnail}
+                toggleRefetch={toggleRefetch}
+              />
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FullReadList;
